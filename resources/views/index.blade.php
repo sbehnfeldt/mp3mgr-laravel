29ec96c2c6a6da2e78b18f1f5cb356ec43a81a91
Stephen Behnfeldt <stephen@behnfeldt.pro>
2023-08-10 08:48:10 -0400

    Basic CSS; index page for app
    Global reset; clearfix fix

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>Document</title>
+</head>
+<body>
+<table>
+    <thead>
+    <tr>
+        <th>Title</th>
+        <th>Artist</th>
+        <th>Album</th>
+        <th>Track #</th>
+        <th>Filename</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr><td colspan="5">No data</td></tr>
+    </tbody>
+</table>
+</body>
+</html>
