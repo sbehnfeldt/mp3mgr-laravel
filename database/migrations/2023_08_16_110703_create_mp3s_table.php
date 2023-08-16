56db2fcd73bcd59f6da0a7ca150162502c1e0a84
Stephen <stephen@behnfeldt.pro>
2023-08-16 07:54:17 -0400

    Add model, controller, factory and migration for Artists, Albums and MP3s

---
+++ database/migrations/2023_08_16_110703_create_mp3s_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mp3_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId( 'author_id')->references('id' )->on( 'artists' );
+            $table->foreignId( 'album_id')->nullable()->nullOnDelete()->cascadeOnUpdate();
+
+            $table->string( 'filename' )->unique();
+            $table->string( 'filename_hash' )->unique();
+
+            $table->string( 'title' )->nullable();
+            $table->string( 'year' )->nullable();
+            $table->string( 'publisher' )->nullable();
+            $table->string( 'genre' )->nullable();
+            $table->integer( 'track' )->nullable();
+            $table->string( 'composer' )->nullable();
+            $table->string( 'album_author' )->nullable();
+            $table->bigInteger('length' )->nullable();
+            $table->string( 'ufid_owner')->nullable();
+            $table->string( 'ufid_identifier')->nullable();
+            $table->text( 'comments')->nullable();
+            $table->binary('album_art')->nullable();
+            $table->integer( 'copyright')->nullable();
+            $table->string( 'desc' )->nullable();
+            $table->string( 'encoded' )->nullable();
+            $table->string( 'url' )->nullable();
+            $table->string( 'original_artist' )->nullable();
+            $table->text( 'private_data')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mp3s');
+    }
+};
