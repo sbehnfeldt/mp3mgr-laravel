099ae9301d9e64ec6aad744d0a708ee67fa43746
Stephen <stephen@behnfeldt.pro>
2023-10-09 07:46:35 -0400

    Begin converting/customizing Breeze and Tailwind boilerplate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,18 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
         <!-- Scripts -->
         @vite(['resources/js/app.js'])
     </head>
     <body>
-    <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header>
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
