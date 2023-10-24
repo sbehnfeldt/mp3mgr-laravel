8f25c54aafbd48b0e87b8b8cf73db7fcd3959324
Stephen <stephen@behnfeldt.pro>
2023-10-24 08:31:29 -0400

    API call to fetch an artist by ID, plus albums, plus tracks

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -5,10 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Artist;
 use Illuminate\Http\Request;
 
+
 class ArtistController extends Controller
 {
+
+    // Fetch all Artist records (paginated)
     public function index()
     {
         return Artist::withCount('albums')->paginate(25);
+    }
+
+
+    // Fetch a single Artist record, with corresponding albums and tracks
+    public function show($id)
+    {
+        $artist = Artist::with([
+            'albums' => function ($query) {
+                $query->with('tracks');
+            }
+        ])->findOrFail($id);
+        return $artist;
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
     }
 }
