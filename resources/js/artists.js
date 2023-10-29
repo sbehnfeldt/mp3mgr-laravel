557263735bb35ccfbd6a53ccc1956fe7e0b6ab76
Stephen <stephen@behnfeldt.pro>
2023-10-29 07:09:06 -0400

    Implement paging buttons on "Artists" page

--- resources/js/artists.js
+++ resources/js/artists.js
@@ -5,18 +5,24 @@ import jQuery from 'jquery';
     'use strict';
 
     if (document.querySelector('.artists-page')) {
-        axios.get('/api/artists')
-            .then(response => {
-                buildArtistsTableBody(response.data.data);
-                buildArtistsTableFooter(response.data);
-            })
-            .catch(error => {
-                alert("Error");
-                console.log(error);
-            });
+        fetch('/api/artists');
+
+        function fetch(url) {
+            console.log(url);
+            axios.get(url)
+                .then(response => {
+                    buildArtistsTableBody(response.data.data);
+                    buildArtistsTableFooter(response.data);
+                })
+                .catch(error => {
+                    alert("Error");
+                    console.log(error);
+                });
+        }
 
         function buildArtistsTableBody(artists) {
             let $tbody = $('table.artists tbody');
+            $tbody.empty();
             for (let i = 0; i < artists.length; i++) {
                 let artist = artists[i];
                 let $tr    = $('<tr>');
@@ -36,23 +42,46 @@ import jQuery from 'jquery';
         }
 
         function buildArtistsTableFooter(paging) {
-            console.log( paging );
+            console.log(paging);
             let $tfoot = $('table.artists tfoot');
-            let $tr    = $('<tr>');
+            $tfoot.empty();
+            let $tr = $('<tr>');
 
-            let $td = $('<td colspan="3">');
+            let $td = $('<td colspan="999">');
 
-            let button = $('<button>').text('<<');
+            let button = $('<button class="api">').text('<<');
+            if (1 < paging.current_page) {
+                button.on('click', () => {
+                    fetch(paging.first_page_url)
+                });
+            } else {
+                button.prop('disabled', true);
+            }
             $td.append(button);
-            button = $('<button>').text('<');
+
+            button = $('<button class="api">').text('<');
+            if (1 < paging.current_page) {
+                button.on('click', () => {
+                    fetch(paging.prev_page_url)
+                });
+            } else {
+                button.attr('disabled', 'disabled');
+            }
             $td.append(button);
+
             let span = $(`<span>Page ${paging.current_page} of ${paging.last_page}</span>`)
             $td.append(span);
 
-            button = $('<button>').text('>');
+            button = $('<button class="api">').text('>').on('click', () => {
+                fetch(paging.next_page_url)
+            });
             $td.append(button);
-            button = $('<button>').text('>>');
+
+            button = $('<button class="api">').text('>>').on('click', () => {
+                fetch(paging.last_page_url);
+            });
             $td.append(button);
+
             $tr.append($td);
             $tfoot.append($tr);
         }
