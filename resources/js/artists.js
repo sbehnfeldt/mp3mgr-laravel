83e014f5909c098e0d97afa85faf7f19199a4f2a
Stephen <stephen@behnfeldt.pro>
2023-10-29 06:34:01 -0400

    Refactor JS for "Artists" page
    Broke table-building function into smaller pieces.  No functional changes.

--- resources/js/artists.js
+++ resources/js/artists.js
@@ -7,52 +7,54 @@ import jQuery from 'jquery';
     if (document.querySelector('.artists-page')) {
         axios.get('/api/artists')
             .then(response => {
-                console.log( response.data );
-
-                let artists = response.data.data;
-                let $tbody = $('table.artists tbody');
-                for (let i = 0; i < artists.length; i++) {
-                    let artist = response.data.data[i];
-                    let $tr = $('<tr>');
-                    let $td = $('<td>').text( artist.id );
-                    $tr.append($td);
-
-                    let $a = $('<a>').text( artist.name).attr( 'href', '/artists/' + artist.id);
-                    $td = $('<td>');
-                    $td.append($a);
-                    $tr.append($td);
-
-                    $td = $('<td>' ).text( artist.albums_count );
-                    $tr.append($td);
-
-                    $tbody.append($tr);
-                }
-
-
-                let $tfoot = $('table.artists tfoot');
-                let $tr = $('<tr>');
-
-                let $td = $('<td colspan="3">');
-
-                let button = $( '<button>' ).text( '<<');
-                $td.append( button );
-                button = $( '<button>' ).text( '<');
-                $td.append( button );
-                let span = $(`<span>Page ${response.data.current_page} of ${response.data.last_page}</span>`)
-                $td.append( span );
-
-                button = $( '<button>' ).text( '>');
-                $td.append( button );
-                button = $( '<button>' ).text( '>>');
-                $td.append( button );
-                $tr.append( $td);
-                $tfoot.append( $tr);
-
-
+                buildArtistsTableBody(response.data.data);
+                buildArtistsTableFooter(response.data);
             })
             .catch(error => {
                 alert("Error");
                 console.log(error);
             });
+
+        function buildArtistsTableBody(artists) {
+            let $tbody = $('table.artists tbody');
+            for (let i = 0; i < artists.length; i++) {
+                let artist = artists[i];
+                let $tr    = $('<tr>');
+                let $td    = $('<td>').text(artist.id);
+                $tr.append($td);
+
+                let $a = $('<a>').text(artist.name).attr('href', '/artists/' + artist.id);
+                $td    = $('<td>');
+                $td.append($a);
+                $tr.append($td);
+
+                $td = $('<td>').text(artist.albums_count);
+                $tr.append($td);
+
+                $tbody.append($tr);
+            }
+        }
+
+        function buildArtistsTableFooter(paging) {
+            console.log( paging );
+            let $tfoot = $('table.artists tfoot');
+            let $tr    = $('<tr>');
+
+            let $td = $('<td colspan="3">');
+
+            let button = $('<button>').text('<<');
+            $td.append(button);
+            button = $('<button>').text('<');
+            $td.append(button);
+            let span = $(`<span>Page ${paging.current_page} of ${paging.last_page}</span>`)
+            $td.append(span);
+
+            button = $('<button>').text('>');
+            $td.append(button);
+            button = $('<button>').text('>>');
+            $td.append(button);
+            $tr.append($td);
+            $tfoot.append($tr);
+        }
     }
 })(this, jQuery);
