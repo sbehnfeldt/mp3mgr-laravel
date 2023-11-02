a0c007ed534329597d2ed875f06965751228ca40
Stephen <stephen@behnfeldt.pro>
2023-11-02 12:39:41 -0400

    Construct album list on "Artist" page using Handlebars templating engine for JS.

--- resources/js/artist.js
+++ resources/js/artist.js
@@ -1,46 +1,25 @@
 import jQuery from 'jquery';
 import axios from "axios";
+import Handlebars from "handlebars";
+
 
 ;(function (global, $) {
     'use strict';
 
     if (document.querySelector('.artist-page')) {
-        let url = $(location).attr('href' ).split( '/');
-        let id = url.pop();
-
-
-
-        axios.get('/api/artists/' + id)
-            .then(response => {
-                let artist = response.data;
-                $('h2').text( artist.name );
-
-                let $albumList = $('ul.album-list');
-                for ( let i = 0; i < artist.albums.length; i++ ) {
-                    let album = artist.albums[i];
-                    console.log( album );
-                    let $li = $('<li>').addClass( 'album-title' ).text( album.title );
-                    let $trackList = $('<ul>').addClass( 'track-list');
-
-                    if ( artist.albums.length === i + 1) {
-                        // ref: https://stackoverflow.com/questions/13554552/why-does-classlast-of-type-not-work-as-i-expect
-                        $trackList.addClass( 'final' );
-                    }
-                    for ( let j = 0; j < album.tracks.length; j++ ) {
-                        let track = album.tracks[j];
-                        // console.log( track );
-                        let $li2 = $('<li>').text( track.title );
-                        $trackList.append($li2);
-                    }
-                    $li.append($trackList);
-                    $albumList.append( $li );
-                }
-
-            })
-            .catch(error => {
-                alert("Error");
-                console.log(error);
-            });
-
+        // Fetch API data for the artist identified (by artist ID) in the URL
+        // and use that data to build the album list
+        (function (url) {
+            axios.get(url)
+                .then(response => {
+                    let artist = response.data;
+                    $('h2').text(artist.name);
+                    $('ul.album-list')[0].innerHTML = Handlebars.compile($('#album-list-item-template')[0].innerHTML)(artist);
+                })
+                .catch(error => {
+                    alert("Error");
+                    console.log(error);
+                });
+        })('/api/artists/' + $(location).attr('href').split('/').pop())
     }
 })(this, jQuery);
--- resources/views/artist.blade.php
+++ resources/views/artist.blade.php
@@ -1,5 +1,22 @@
 <x-app-layout>
     <x-slot name="pageClass">artist-page</x-slot>
-    <h2></h2>
-    <ul class="album-list" />
+    <h2>Artist Page</h2>
+    <ul class="album-list"/>
+
+    @verbatim
+        <script id="album-list-item-template" type="text/x-handlebars-template">
+            {{#each albums}}
+            <li class="album {{#if @last}} final-album {{/if}}">
+                <ul class="track-list">{{title}}
+                    {{#each tracks}}
+                    <li class="track">{{title}}</li>
+                    {{/each}}
+                </ul>
+            </li>
+            {{/each}}
+        </script>
+
+        <script id="track-list-item-template" type="text/x-handlebars-template">
+        </script>
+    @endverbatim
 </x-app-layout>
